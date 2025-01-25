f107052ea3285623f41b6823bc01d72bfa7dc506
Ruslan luffee <aliullov2016@mail.ru>
2025-01-25 13:35:18 +0300

    Спринт 6

--- src/manager/Manager.java
+++ src/manager/Manager.java
@@ -7,6 +7,7 @@ public class Manager  {
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
+
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
